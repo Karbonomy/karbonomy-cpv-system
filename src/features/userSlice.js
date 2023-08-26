5b0b66523d30b8528ffe91d48171efffd911b4e6
minhtan <32776120+minhtan4711@users.noreply.github.com>
2023-08-26 16:32:29 +0700

    feature: add carbon amount to company

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -4,7 +4,8 @@ const initialState = {
   name: '',
   email: '',
   wallet: '',
-  isConnected: false
+  isConnected: false,
+  carbonAmount: 0,
 }
 
 const userSlice = createSlice({
@@ -12,17 +13,19 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     setLoggedInUser: (state, action) => {
-      const { name, email, wallet } = action.payload;
+      const { name, email, wallet, carbonAmount } = action.payload;
       state.name = name;
       state.email = email;
       state.wallet = wallet;
-      state.isConnected = true
+      state.isConnected = true;
+      state.carbonAmount = carbonAmount;
     },
     clearLoggedInUser: (state) => {
       state.email = '';
       state.name = '';
       state.wallet = '';
-      state.isConnected = false
+      state.isConnected = false;
+      state.carbonAmount = 0;
     }
   }
 })
